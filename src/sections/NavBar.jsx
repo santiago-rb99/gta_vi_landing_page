58617473cbd837473257f286663c842d250aa9f9
Santiago <santiagorb1542@gmail.com>
2026-06-04 15:43:28 -0400

    Implement Hero Section

---
+++ src/sections/NavBar.jsx
@@ -0,0 +1,10 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <img src="/images/nav-logo.svg" className="scale-90" alt="gta logo" />
+      <img src="/images/menu.svg" className="w-10" alt="hamburger menu" />
+    </nav>
+  );
+};
+
+export default NavBar;
